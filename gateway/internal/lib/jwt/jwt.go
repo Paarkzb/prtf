4210715f06c83cbfb1ada41f3dcf0ed4b7e3066b
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2024-10-08 22:11:06 +0800

    change parse method for token

--- gateway/internal/lib/jwt/jwt.go
+++ gateway/internal/lib/jwt/jwt.go
@@ -4,36 +4,25 @@ import (
 	"errors"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 )
 
 const (
 	signingKey = "hwekjf#hadsujfDPDSFJO21adho@JDSOV*@79Q"
 )
 
-type tokenClaims struct {
-	jwt.RegisteredClaims
-	UserId uuid.UUID `json:"user_id"`
-}
+func ParseToken(accessToken string) (jwt.MapClaims, error) {
 
-func ParseToken(accessToken string) (tokenClaims, error) {
-	var claims tokenClaims
-
-	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("invalid signing method")
-		}
-
+	token, err := jwt.Parse(accessToken, func(t *jwt.Token) (interface{}, error) {
 		return []byte(signingKey), nil
 	})
 
 	if err != nil {
-		return claims, err
+		return nil, err
 	}
 
-	claims, ok := token.Claims.(tokenClaims)
+	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
-		return claims, errors.New("token claims are not of type *tokenClaims")
+		return nil, errors.New("token claims are not of type *tokenClaims")
 	}
 
 	return claims, nil
