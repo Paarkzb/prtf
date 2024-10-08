27903fb1fc3ae1ff095cb4d895ef315f95fe5bb5
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2024-10-08 22:09:25 +0800

    jwt change creating token

--- auth/sso/internal/lib/jwt/jwt.go
+++ auth/sso/internal/lib/jwt/jwt.go
@@ -5,26 +5,20 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 )
 
 const (
 	signingKey = "hwekjf#hadsujfDPDSFJO21adho@JDSOV*@79Q"
 )
 
-type tokenClaims struct {
-	jwt.RegisteredClaims
-	UserId uuid.UUID `json:"user_id"`
-}
-
 func NewToken(user models.User, duration time.Duration) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
-		jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-		},
-		user.ID,
-	})
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["uid"] = user.ID
+	claims["username"] = user.Username
+	claims["exp"] = time.Now().Add(duration).Unix()
+	claims["iat"] = time.Now().Unix()
 
 	tokenString, err := token.SignedString([]byte(signingKey))
 	if err != nil {
