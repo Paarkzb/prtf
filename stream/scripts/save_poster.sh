952ef93185239231f90077d6e0dcab40c12ff0ba
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2025-03-06 02:54:06 +0800

    channel page style

--- stream/scripts/save_poster.sh
+++ stream/scripts/save_poster.sh
@@ -3,6 +3,6 @@ PATH=$1
 CHANNEL_NAME=$2
 
 /usr/bin/ffmpeg -i "/var/rec/${PATH}/${CHANNEL_NAME}.m3u8" -ss 00:00:01 -vframes 1 -q:v 2 "/var/rec/${PATH}/poster.jpg"
-/usr/bin/magick /var/rec/${PATH}/poster.jpg -resize 290x160 -quality 80 /var/rec/${PATH}/poster.jpg
+/usr/bin/magick /var/rec/${PATH}/poster.jpg -resize 640x360 -quality 80 /var/rec/${PATH}/poster.jpg
 
 echo -n "${PATH}/poster.jpg"
